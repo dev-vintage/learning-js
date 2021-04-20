263b05f998c0d36ae421ff1defaab90513133263
dev-calaca <contato.kalassaprod@gmail.com>
2021-04-20 17:32:35 -0300

    finished the first lesson

---
+++ Lesson01/app.js
@@ -0,0 +1,27 @@
+// Insert a message on console
+
+console.log("Hello, World!");
+
+// Variables 
+
+let firstName = 'José';
+console.log(firstName);
+
+let lastName = 'Gomes';
+console.log(lastName);
+
+let age = 15;
+let fullAge = false;
+
+// other alternative to declare a variable
+let job;
+job = 'Student';
+
+/*
+ * Variable mutation and type coercion
+ */
+
+lastName = "Calaça Gomes";
+console.log(firstName + ' ' + lastName);
+
+
