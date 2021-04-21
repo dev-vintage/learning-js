96b9e489da782346c74bcefca7c7638b8bf910b1
dev-calaca <contato.kalassaprod@gmail.com>
2021-04-21 10:31:12 -0300

    finished the lesson04 about boolean logic

---
+++ Lesson04/app.js
@@ -0,0 +1,14 @@
+/*
+ * Boolean Logic
+ */
+
+let firstName = 'José';
+let age = 15;
+
+if (age < 13){
+	console.log(firstName + ' is a boy');
+} else if(age >= 13 && age < 20){
+	console.log(firstName + ' is a teenager')
+} else{
+	console.log(firstName + ' is a man');
+}
