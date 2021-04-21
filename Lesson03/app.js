00c4245fb34f3e54b7b553fd4146240a29bc9c56
dev-calaca <contato.kalassaprod@gmail.com>
2021-04-21 10:21:24 -0300

    finished lesson03 about if else statements

---
+++ Lesson03/app.js
@@ -0,0 +1,23 @@
+/*
+ * if / else statements
+ */
+
+let firstName   = 'José';
+let civilStatus = 'single';
+
+if(civilStatus === 'married'){
+	console.log(firstName + ' is married!');
+
+} else {
+	console.log(firstName + ' will hopefully marry soon :)');
+}
+
+let isMarried = false;
+
+if(isMarried){
+	console.log(firstName + ' is married!');
+} else{
+	console.log(firstName + ' will hopefully marry soon!');
+}
+
+
