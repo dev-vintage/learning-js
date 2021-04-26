fbe92cf43841420913229087d182ef7ad2a1fd8f
dev-calaca <contato.kalassaprod@gmail.com>
2021-04-26 13:41:06 -0300

    loops and iteration

---
+++ Lesson10/app.js
@@ -0,0 +1,30 @@
+/*
+ * Loops an iteration
+ */
+
+/*
+ * for loop
+ */
+
+for (var i = 0; i < 10; i++){
+	console.log(i);
+}
+
+let john = ['John', 'Smith', 1990, 'designer', false];
+
+for (var i = 0; i < john.length; i++){
+	console.log(john[i]);
+}
+
+/*
+ * while loop
+ */
+
+var i = 0;
+
+while(i < john.length){
+	console.log(john[i]);
+	i++;
+}
+
+
