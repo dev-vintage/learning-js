c1c2face0a706f93e7f3a339ae436c560a8ce0f3
dev-calaca <contato.kalassaprod@gmail.com>
2021-05-18 11:54:48 -0300

    Infinity Description

--- Value-Properties/infinity.js
+++ Value-Properties/infinity.js
@@ -27,3 +27,16 @@ console.log(Math.pow(10, 1000)); /* Infinity */
 console.log(Math.log(0)       ); /* -Infinity */
 console.log(1 / Infinity      ); /* 0 */
 console.log(1 / 0             ); /* Infinity */
+
+/*
+ * Description
+ * Infinity is a property of the global object. In other words, it is a variable in global scope.
+ *
+ * The initial value of Infinity is Number.POSITIVE_INFINITY. The value Infinity (positive infinity) is greater 
+ * than any other number.
+ *
+ * This value behaves slightly differently than mathematical infinity; see Number.POSITIVE_INFINITY for details.
+ *
+ * As defined by the ECMAScript 5 specification, Infinity is read-only (implemented in JavaScript 1.8.5 / Firefox 4).
+ *
+ */
