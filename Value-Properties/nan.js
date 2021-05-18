9c6c1f0b6ea224e74a602b5cedbb26bc2829f586
dev-calaca <contato.kalassaprod@gmail.com>
2021-05-18 12:05:03 -0300

    NaN Description

--- Value-Properties/nan.js
+++ Value-Properties/nan.js
@@ -22,3 +22,19 @@ console.log(sanitise('1'));
 
 console.log(sanitise('NotANumber'));
 // expected output: NaN
+
+/*
+ * NaN is a property of the global object. In other words, it is a variable in global scope.
+ *
+ * The initial value of NaN is Not-A-Number — the same as the value of Number.NaN. In modern browsers, NaN is a
+ * non-configurable, non-writable property. Even when this is not the case, avoid overriding it. It is rather 
+ * rare to use NaN in a program.
+ *
+ * There are five different types of operations that return NaN:
+ *
+ * Number cannot be parsed (e.g. parseInt("blabla") or Number(undefined))
+ * Math operation where the result is not a real number (e.g. Math.sqrt(-1))
+ * Operand of an argument is NaN (e.g. 7 ** NaN)
+ * Indeterminate form (e.g. 0 * Infinity, or undefined + undefined)
+ * Any operation that involves a string and is not an addition operation (e.g. "foo" / 3)
+ */
