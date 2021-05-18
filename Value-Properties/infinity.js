6d499ef6d1b0fb3a4b4eccca49761f479f7d922f
dev-calaca <contato.kalassaprod@gmail.com>
2021-05-17 21:02:38 -0300

    Using Infinity

--- Value-Properties/infinity.js
+++ Value-Properties/infinity.js
@@ -21,3 +21,9 @@ if (maxNumber === Infinity){
 console.log(1 / maxNumber);
 // expected output: 0
 
+console.log(Infinity          ); /* Infinity */
+console.log(Infinity + 1      ); /* Infinity */
+console.log(Math.pow(10, 1000)); /* Infinity */
+console.log(Math.log(0)       ); /* -Infinity */
+console.log(1 / Infinity      ); /* 0 */
+console.log(1 / 0             ); /* Infinity */
