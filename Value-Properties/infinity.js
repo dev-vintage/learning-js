81cc9cc432e3d0e50cc37af2aac230d7826a82dc
dev-calaca <contato.kalassaprod@gmail.com>
2021-05-17 21:01:18 -0300

    JavaScipt Demo : Standard built-in objects - infinity

--- Value-Properties/infinity.js
+++ Value-Properties/infinity.js
@@ -6,3 +6,18 @@
  * Configurable -- no
  *
  */
+
+/*
+ * JavaScript Demo: Standard built-in objects - infinity
+ */
+
+const maxNumber = Math.pow(10, 100); // max positive number
+
+if (maxNumber === Infinity){
+  console.log('Let\'s call it Infinity');
+  // expected output: "Let's call it Infinity!"
+}
+
+console.log(1 / maxNumber);
+// expected output: 0
+
