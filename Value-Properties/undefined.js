3943fc06a7ce371c27e386a2f6ae1b6f63879599
dev-calaca <contato.kalassaprod@gmail.com>
2021-05-19 13:40:39 -0300

    Undefined Description

--- Value-Properties/undefined.js
+++ Value-Properties/undefined.js
@@ -23,3 +23,15 @@ let x;
 console.log(test(x));
 // expected output: "undefined value!";
 
+/*
+undefined is a property of the global object. That is, it is a variable in global scope. The initial value of 
+undefined is the primitive value undefined.
+
+In modern browsers (JavaScript 1.8.5 / Firefox 4+), undefined is a non-configurable, non-writable property,
+per the ECMAScript 5 specification. (Even when this is not the case, avoid overriding it.)
+
+A variable that has not been assigned a value is of type undefined. A method or statement also returns undefined 
+if the variable that is being evaluated does not have an assigned value. A function returns undefined if a value 
+was not returned.
+
+*/
