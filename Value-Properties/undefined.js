3e1dbb79ba5c92f96a2e56f18c35a08fa905ee9f
dev-calaca <contato.kalassaprod@gmail.com>
2021-05-19 13:38:59 -0300

    Demo Undefined Value

--- Value-Properties/undefined.js
+++ Value-Properties/undefined.js
@@ -7,6 +7,19 @@ Writable	no
 Enumerable	no
 Configurable	no
 
-
-
 */
+
+/*JavaScript Demo: Standard built-in objects - undefined*/
+
+function test(t) {
+	if (t == undefined) {
+		return 'undefined value!';
+	}
+	return t;
+}
+
+let x;
+
+console.log(test(x));
+// expected output: "undefined value!";
+
