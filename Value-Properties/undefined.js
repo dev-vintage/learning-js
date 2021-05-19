8ef042e66b0a49b8e65e5db160aaf042580cdd3f
dev-calaca <contato.kalassaprod@gmail.com>
2021-05-19 13:35:28 -0300

    Undefined Value Propertie

---
+++ Value-Properties/undefined.js
@@ -0,0 +1,12 @@
+/*
+
+The global undefined property represents the primitive value undefined. It is one of JavaScript's primitive types.
+
+Property attributes of undefined
+Writable	no
+Enumerable	no
+Configurable	no
+
+
+
+*/
